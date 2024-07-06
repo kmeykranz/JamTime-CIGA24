2bb52bcd6d128aac9604f62275d2ddacc5f0c682
MamboJiang <mambojiang@icloud.com>
2024-07-06 13:50:00 +0800

    Add play manga scene

---
+++ scripts/manga/start_manga.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	var display_manga = preload("res://scenes/display_manga.tscn").instantiate()
+	
+	# 设置自定义路径
+	display_manga.left_manga_path = "res://assets/samplemanga.png"
+	display_manga.middle_manga_path = "res://assets/samplemanga.png"
+	display_manga.right_manga_path = "res://assets/samplemanga.png"
+	display_manga.next_scene_path = "res://scenes/title_screen.tscn"
+	
+	add_child(display_manga)
